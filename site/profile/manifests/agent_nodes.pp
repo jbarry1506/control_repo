322c8363e240e56a8d17499a5cd521b315e61fb3
jbarry1506 <jbarry1506@gmail.com>
2018-08-29 20:28:00 -0400

    include docker

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -1,5 +1,8 @@
 class profile::agent_nodes {
   include dockeragent
+  include docker
+  docker::node {'web.puppet.vm'}
+  docker::node {'db.puppet.vm'}
   dockeragent::node {'web.puppet.vm':}
   dockeragent::node {'db.puppet.vm':}
 }
