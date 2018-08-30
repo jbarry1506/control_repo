96d56a34da7eedc43f9343f22fc36ff9014faf03
jbarry1506 <jbarry1506@gmail.com>
2018-08-29 21:20:25 -0400

    Remove docker include lines

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -1,8 +1,5 @@
 class profile::agent_nodes {
   include dockeragent
-  include docker
-  docker::node {'web.puppet.vm'}
-  docker::node {'db.puppet.vm'}
   dockeragent::node {'web.puppet.vm':}
   dockeragent::node {'db.puppet.vm':}
 }
